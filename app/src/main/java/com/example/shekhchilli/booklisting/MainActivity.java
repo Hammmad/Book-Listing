93922ee3edcb778e8c0dc7f82df2067ecc844107
Hammad <hammmad220@gmail.com>
2016-09-25 08:18:55 -0700

    API is working Fine now!!

--- app/src/main/java/com/example/shekhchilli/booklisting/MainActivity.java
+++ app/src/main/java/com/example/shekhchilli/booklisting/MainActivity.java
@@ -33,24 +33,24 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     final String DEBUG_TAG = "DebuGGing";
     TextView message;
-
-
+    String keyword;
     EditText book;
-    private static String apiurl = "https://www.googleapis.com/books/v1/volumes?q=search+terms";
+    String word;
+    private static String apiurl = "https://www.googleapis.com/books/v1/volumes?q=";
     ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        book = (EditText) findViewById(R.id.book_edittext);
         SearchClickListener();
     }
 
     public void SearchClickListener() {
 
-        book = (EditText) findViewById(R.id.book_edittext);
+        message = (TextView) findViewById(R.id.message);
         Button searchbtn = (Button) findViewById(R.id.button);
-       message = (TextView) findViewById(R.id.message);
         searchbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,10 +58,9 @@ public class MainActivity extends AppCompatActivity {
                 NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
                 if (networkInfo != null && networkInfo.isConnected()) {
                     // Write code if internrt is working
+                    keyword = book.getText().toString();
                     message.setText("Internet is Connected");
-                    new API().execute(apiurl);
-
-
+                    new API().execute(apiurl + keyword);
                 } else {
                     message.setText("Internet is not Connected");
                 }
@@ -71,18 +70,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-    private class API extends AsyncTask<String, Void ,String> {
+    private class API extends AsyncTask<String, Void, String> {
 
         String result;
 
         @Override
         protected String doInBackground(String... params) {
-                //String Url = "https://www.googleapis.com/books/v1/volumes?q=";
-                InputStream stream = null;
+            //String Url = "https://www.googleapis.com/books/v1/volumes?q=";
+            InputStream stream = null;
+            BufferedReader reader = null;
+
             try {
-                URL url = new URL(apiurl);
-                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                URL url = new URL(apiurl + keyword);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setReadTimeout(10000);
                 conn.setConnectTimeout(15000);
                 conn.setRequestMethod("GET");
@@ -90,9 +90,16 @@ public class MainActivity extends AppCompatActivity {
 
                 conn.connect();
                 int response = conn.getResponseCode();
-                stream = (InputStream)conn.getContent();
+                stream = conn.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream, "utf-8"), 8);
+                StringBuilder stringBuilder = new StringBuilder();
 
-
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+                stream.close();
+                result = stringBuilder.toString();
 
 
             } catch (MalformedURLException e) {
@@ -100,32 +107,32 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            // Convert Stream into String us String builder
+            // Convert Stream into String using String builder
 
-            try{
-                BufferedReader bReader = new BufferedReader(new InputStreamReader(stream,"utf-8"),8);
-                StringBuilder stringBuilder = new StringBuilder();
-
-                String line = null;
-                while((line = bReader.readLine()) != null){
-                    stringBuilder.append(line +"\n");
-                }
-                stream.close();
-                result = stringBuilder.toString();
-
-            }catch (Exception e){
-                Log.e(DEBUG_TAG,e.getMessage());
-            }
+//            try{
+//                BufferedReader bReader = new BufferedReader(new InputStreamReader(stream,"utf-8"),8);
+//                StringBuilder stringBuilder = new StringBuilder();
+//
+//                String line = null;
+//                while((line = bReader.readLine()) != null){
+//                    stringBuilder.append(line +"\n");
+//                }
+//                stream.close();
+//                result = stringBuilder.toString();
+//
+//            }catch (Exception e){
+//                Log.e(DEBUG_TAG,e.getMessage());
+//            }
             return result;
 
         }
 
         @Override
-        protected void onPostExecute(String FromStream) {
+        protected void onPostExecute(String s) {
 
-            String s = new String(FromStream);
+//            String s = new String(FromStream);
 
-            Log.e(DEBUG_TAG,s);
+            Log.e(DEBUG_TAG, s);
             ArrayList<String> arrayList = new ArrayList<String>();
             try {
                 JSONObject rootObject = new JSONObject(s);
@@ -139,25 +146,25 @@ public class MainActivity extends AppCompatActivity {
                     String title = volume.getString("title");
 
                     arrayList.add(title);
-                    }
+                }
 
 
                 Log.e(DEBUG_TAG, String.valueOf(JArray.length()));
 
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,arrayList);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
                 ListView listView = (ListView) findViewById(R.id.list);
                 listView.setAdapter(adapter);
                 message.setText("code completed");
 
 
             } catch (JSONException e) {
-                Log.e(DEBUG_TAG,e.getMessage());
+                Log.e(DEBUG_TAG, e.getMessage());
                 e.getLocalizedMessage();
             }
 
 
         }
     }
-    }
+}
 
 
